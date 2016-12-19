c46408800477d1f21a2a765ad52a920a5e077f29
jumpei.yamaguchi <jumpei.yamaguchi@speee.jp>
2016-12-19 18:56:51 +0900

    取り急ぎ

---
+++ Gruntfile.js
@@ -0,0 +1,121 @@
+module.exports = function(grunt) {
+    // Gruntの設定
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json')
+    });
+
+    // 各タスクの設定
+    grunt.initConfig({
+        // Webサーバの設定
+        connect: {
+            pc: {
+                options: {
+                    port: 9001,
+                    base: 'www/pc'
+                }
+            }
+        },
+        // clean
+        clean: {
+            options: {
+                force: true
+            },
+            publish: {
+                src: [
+                    'www/**/*'
+                ]
+            }
+        },
+        // copy
+        copy: {
+            source: {
+                expand: true,
+                cwd: 'assets',
+                src: '**/*',
+                dest: 'www'
+            }
+        },
+        // pug
+        pug: {
+            options: {
+                pretty: true,
+                data: grunt.file.readJSON('package.json'),
+                data: function(dest, src) {
+                    var depth = src[0].split('/').length;
+                    var root_dir = '';
+                    var filedepth = depth-3;
+                    for(var i = 0;i < filedepth;i++){
+                        root_dir += '../';
+                    }
+                    return {
+                        from: src,
+                        to: dest,
+                        root_dir:root_dir
+                    };
+                }
+            },
+            source: {
+                expand: true,
+                cwd: 'pug',
+                src: '**/!(_)*.pug',
+                dest: 'www',
+                ext: '.html'
+            }
+        },
+        sass: {
+            pc : {
+                src : 'scss/pc/style.scss',
+                dest : 'www/pc/css/style.css'
+            }
+        },
+        cssmin : {
+            pc : {
+                src : 'www/pc/css/style.css',
+                dest : 'www/pc/css/style.min.css'
+            }
+        },
+        // Watch
+        watch: {
+            // options
+            options: {
+                livereload: true,
+                nospawn: true
+            },
+            // pug
+            pug: {
+                files: 'pug/**/*.pug',
+                tasks: ['pug']
+            },
+            // scss
+            sass: {
+                files: 'scss/**/*.scss',
+                tasks: ['sass', 'cssmin']
+            },
+            // scss
+            assets: {
+                files: 'assets/**/*',
+                tasks: ['copy']
+            }
+        },
+        open: {
+            pc: {
+                path: 'http://localhost:<%= connect.pc.options.port %>/'
+            }
+        }
+    });
+
+    // プラグインの読み込み
+    grunt.loadNpmTasks('grunt-contrib-pug');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-combine-media-queries');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-open');
+
+    // defaultタスクの設定
+    grunt.registerTask('default', ['clean', 'copy', 'pug', 'sass', 'cssmin', 'connect', 'open', 'watch']);
+};
